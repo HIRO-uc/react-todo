629689526cb81c57129226c14226f4e726002522
HIRO-uc <75469488+HIRO-uc@users.noreply.github.com>
2021-02-02 12:46:13 +0900

    reactでのtodo実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,71 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  // incompleteTodosは初期値として['ああああ', 'いいいい']の配列を持つ。
+  const [todoText, setTodoText] = useState("");
+  // incompleteTodosは初期値として[]の配列を持つ。
   // setIncompleteTodosはそれを変化させるための関数
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "ああああ",
-    "いいいい"
-  ]);
-  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    // if文も処理が一行で終わる時は{}を書かなくても良い
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // spliceメソッドは第一引数に配列の始点、第二引数にそこから何個削除するかを設定する
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickUndo = (index) => {
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     // jsx記法ではclassNameでクラスを指定する
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               // mapなどでループしてレンダリングするときは、keyを設定する
               // reactは差分のみを仮想DOMに反映していくため、何個めの要素なのかを判定しなくてはならないから
               // keyを書かないとwarningが出る
+              // 関数に引数を渡す場合はそのままか書くのではなく、アロー関数で新たに関数を生成する必要がある
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -36,11 +75,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了したTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickUndo(index)}>戻す</button>
               </div>
             );
           })}
