0d0f48e572c840af0669eb1a89ebaeef21f87286
HIRO-uc <75469488+HIRO-uc@users.noreply.github.com>
2021-02-02 13:36:53 +0900

    完了リストのコンポーネント化

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { completeTodos, onClickUndo } = props;
+
+  return (
+    <div className="complete-area">
+      <p className="title">完了したTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickUndo(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
